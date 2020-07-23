d2c94a0eecf4bbf040cfd8e4a6f13124f28fac6b
Hise <jacksondvlog@gmail.com>
2020-07-23 16:06:03 -0300

    First commit

---
+++ Gerador-Nitro/nitro.py
@@ -0,0 +1,44 @@
+import time
+import random
+
+print("GERADOR DE NITRO:")
+
+i = 0
+
+cod = ["a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l",
+"L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"]
+
+print("Ativacao de nitro quase concluida...")
+time.sleep(2)
+print("Ativacao completa...")
+time.sleep(3)
+
+while i < 1000:
+  r1 = random.randrange(0, 24)
+  r2 = random.randrange(0, 24)
+  r3 = random.randrange(0, 24)
+  r4 = random.randrange(3, 24)
+  r5 = random.randrange(0, 24)
+  r6 = random.randrange(0, 24)
+  r7 = random.randrange(0, 24)
+  r8 = random.randrange(0, 24)
+  r9 = random.randrange(0, 24)
+  r10 = random.randrange(0, 24)
+  r11 = random.randrange(0, 24)
+  r12 = random.randrange(0, 24)
+  r13 = random.randrange(0, 24)
+  r14 = random.randrange(0, 24)
+  r15 = random.randrange(0, 24)
+  r16 = random.randrange(0, 24)
+  r17 = random.randrange(0, 24)
+  r18 = random.randrange(0, 24)
+  r19 = random.randrange(0, 24)
+  r20 = random.randrange(0, 24)
+  r21 = random.randrange(0, 24)
+  r22 = random.randrange(0, 24)
+  r23 = random.randrange(0, 24)
+  r24 = random.randrange(0, 24)
+  
+  print("https://discordapp.com/gifts/" + cod[r1] + cod[r2] + cod[r3] + cod[r4] + cod[r5] + cod[r6] + cod[r7] + cod[r8] + cod[r9] + cod[r10] + cod[r11] + cod[r12] + cod[r13] + cod[r14] + cod[r15] + cod[r16] + cod[r17] + cod[r18] + cod[r19] + cod[r20] + cod[r21] + cod[r22] + cod[r23] + cod[r24])
+  
+ 
