d2ccfef811ef880f5f5fe9b4ecb23d156b314464
HI23DC <68709123+HI23DC@users.noreply.github.com>
2020-07-24 11:59:13 -0300

    Update nitro.py

--- Gerador-Nitro/nitro.py
+++ Gerador-Nitro/nitro.py
@@ -11,7 +11,7 @@ print("                         => Criador: Hi23")
 
 i = 0
 
-cod = ["a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l",
+cod = ["a", "A", "0" "b", "B", "1", "c", "C", "2", "d", "D", "3", "e", "E", "4", "f", "F", "5", "g", "G", "6", "h", "H", "7", "i", "I", "8" "j", "J", "9", "k", "K", "l",
 "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"]
 
 print("Ativacao de nitro quase concluida...")
@@ -20,29 +20,30 @@ print("Ativacao completa...")
 time.sleep(3)
 
 while i < 500:
-  r1 = random.randrange(0, 24)
-  r2 = random.randrange(0, 24)
-  r3 = random.randrange(0, 24)
-  r4 = random.randrange(3, 24)
-  r5 = random.randrange(0, 24)
-  r6 = random.randrange(0, 24)
-  r7 = random.randrange(0, 24)
-  r8 = random.randrange(0, 24)
-  r9 = random.randrange(0, 24)
-  r10 = random.randrange(0, 24)
-  r11 = random.randrange(0, 24)
-  r12 = random.randrange(0, 24)
-  r13 = random.randrange(0, 24)
-  r14 = random.randrange(0, 24)
-  r15 = random.randrange(0, 24)
-  r16 = random.randrange(0, 24)
-  r17 = random.randrange(0, 24)
-  r18 = random.randrange(0, 24)
-  r19 = random.randrange(0, 24)
-  r20 = random.randrange(0, 24)
-  r21 = random.randrange(0, 24)
-  r22 = random.randrange(0, 24)
-  r23 = random.randrange(0, 24)
-  r24 = random.randrange(0, 24)
+  r1 = random.randrange(0, 62)
+  r2 = random.randrange(0, 62)
+  r3 = random.randrange(0, 62)
+  r4 = random.randrange(0, 62)
+  r5 = random.randrange(0, 62)
+  r6 = random.randrange(0, 62)
+  r7 = random.randrange(0, 62)
+  r8 = random.randrange(0, 62)
+  r9 = random.randrange(0, 62)
+  r10 = random.randrange(0, 62)
+  r11 = random.randrange(0, 62)
+  r12 = random.randrange(0, 62)
+  r13 = random.randrange(0, 62)
+  r14 = random.randrange(0, 62)
+  r15 = random.randrange(0, 62)
+  r16 = random.randrange(0, 62)
+  r17 = random.randrange(0, 62)
+  r18 = random.randrange(0, 62)
+  r19 = random.randrange(0, 62)
+  r20 = random.randrange(0, 62)
+  r21 = random.randrange(0, 62)
+  r22 = random.randrange(0, 62)
+  r23 = random.randrange(0, 62)
+  r24 = random.randrange(0, 62)
   print("https://discord.com/gifts/" + cod[r1] + cod[r2] + cod[r3] + cod[r4] + cod[r5] + cod[r6] + cod[r7] + cod[r8] + cod[r9] + cod[r10] + cod[r11] + cod[r12] + cod[r13] + cod[r14] + cod[r15] + cod[r16] + cod[r17] + cod[r18] + cod[r19] + cod[r20] + cod[r21] + cod[r22] + cod[r23] + cod[r24]);
   
+  # https://discord.com/gifts/duHW5XPgE8AYsvWD2qCZ4EcH
